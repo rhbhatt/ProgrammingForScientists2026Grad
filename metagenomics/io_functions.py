d55910afcc5c9ec0cf9e9694cc70446862599ca1
Roshni Bhatt <39613541+rhbhatt@users.noreply.github.com>
2026-08-26 17:55:23 -0400

    IO template for metagenomics

---
+++ metagenomics/io_functions.py
@@ -0,0 +1,13 @@
+import os
+
+def ReadSamplesFromDirectory(directory):
+    return
+
+def ReadFrequencyMapFromFile(filename):
+    return
+
+def WriteBetaDiversityMatrixToFile(mtx, sample_names, filename):
+    return
+
+def WriteSimpsonsMapToFile(simpson, filename):
+    return
